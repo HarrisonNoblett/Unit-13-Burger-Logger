7e9fca960c4adfbb4322f11cf4bd878debd5c558
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-01-22 18:43:13 -0600

    created database and installed all required installations

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', True);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss Burger', false);
