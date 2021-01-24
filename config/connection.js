d8dba366922ebbe3b70a6a05fded61361ce4e9bf
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-01-23 19:12:25 -0600

    finished the orm file

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: "cat_db"
+  password: "Panda736!",
+  database: "burger_db"
 });
 
 // Makeing connection and console.logging.
